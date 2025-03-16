b5d38e269550d33969518b7ee5e68d57d4d47ac5
Nandang Hermawan 55 <nandanghermawan55@gmail.com>
2025-03-16 22:49:27 +0700

    update

--- lib/my_image_picker.dart
+++ lib/my_image_picker.dart
@@ -400,7 +400,7 @@ class ImagePickerComponent extends StatelessWidget {
     if (value.onProgressUpload == false &&
         value.state == ImagePickerComponentState.Enable) {
       controller.uploadFile(
-        value.isUploaded && getUpdateUrl != null
+        value.loadData && getUpdateUrl != null
             ? getUpdateUrl!(value)
             : uploadUrl ?? "",
         uploadField ?? "file",
