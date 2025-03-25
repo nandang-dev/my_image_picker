be381cac50ed0b2f27cca1becded7d1bae2b2ce4
Nandang Hermawan 55 <nandanghermawan55@gmail.com>
2025-03-26 06:01:57 +0700

    update setOnUploadQueryParams

--- lib/my_multiple_image_picker.dart
+++ lib/my_multiple_image_picker.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -21,7 +22,7 @@ class MultipleImagePickerComponent extends StatelessWidget {
   final WidgetFromDataBuilder2<VoidCallback, VoidCallback>? popUpChild;
   final WidgetFromDataBuilder<VoidCallback>? buttonCamera;
   final WidgetFromDataBuilder<VoidCallback>? buttonGalery;
-  final ObjectBuilderWithParam<Map<String, String>, ImagePickerValue>?
+  final ObjectBuilderWith2Param<Map<String, String>, ImagePickerValue, int>?
       setOnUploadQueryParams;
   final BoxDecoration? popUpDecoration;
   final EdgeInsetsGeometry? popUpMargin;
@@ -184,7 +185,11 @@ class MultipleImagePickerComponent extends StatelessWidget {
                 deleteUrl: deleteUrl,
                 token: token,
                 canReupload: canReupload,
-                setOnUploadQueryParams: setOnUploadQueryParams,
+                setOnUploadQueryParams: setOnUploadQueryParams == null
+                    ? null
+                    : (value) {
+                        return setOnUploadQueryParams?.call(value, index) ?? {};
+                      },
                 useDocumentPicker: useDocumentPicker,
                 onUploaded: (val) {
                   controller.setState(() {
